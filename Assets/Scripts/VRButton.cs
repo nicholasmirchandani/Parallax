a9ba0682aac41bab8477dfb533a41c1d28fd2856
Nicholas Mirchandani <nmirchandani@chapman.edu>
2019-10-29 19:33:28 -0700

    Implements and Cleans up Lever along with style updates to follow style guide

--- Assets/Scripts/VRButton.cs
+++ Assets/Scripts/VRButton.cs
@@ -11,8 +11,7 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class VRButton : MonoBehaviour
-{
+public class VRButton : MonoBehaviour {
     public UnityEvent buttonPress;
     public UnityEvent buttonRelease;
     public GameObject button;
@@ -26,29 +25,24 @@ public class VRButton : MonoBehaviour
     //TODO: Implement Custom Spring implementation to allow use of FixedUpdate instead of Update.  Currently, Spring Joints run after FixedUpdate code, nullifying any physics programming we'd do there.
 
     //Runs once per game frame
-    private void Update()
-    {
+    private void Update() {
         //Disallows x-z movement
-        if (button.transform.localPosition.x != 0.0f || button.transform.localPosition.z != 0.0f)
-        {
+        if (button.transform.localPosition.x != 0.0f || button.transform.localPosition.z != 0.0f) {
             button.transform.localPosition = new Vector3(0f, button.transform.localPosition.y, 0f);
         }
 
         //Notes Button High/Low value
-        if (!buttonPressed && button.transform.localPosition.y < lowPosTrigger)
-        {
+        if (!buttonPressed && button.transform.localPosition.y < lowPosTrigger) {
             buttonPressed = true;
             buttonPress.Invoke();
         }
-        else if (buttonPressed && button.transform.localPosition.y > highPosTrigger)
-        {
+        else if (buttonPressed && button.transform.localPosition.y > highPosTrigger) {
             buttonPressed = false;
             buttonRelease.Invoke();
         }
 
         //Constrains y movement to bounds set in editor
-        if (button.transform.localPosition.y > maxYPos)
-        {
+        if (button.transform.localPosition.y > maxYPos) {
             button.transform.localPosition = new Vector3(0f, maxYPos, 0f);
         }
 
---
+++ Assets/Scripts/VRLever.cs
@@ -0,0 +1,56 @@
+﻿/************************************************************************
+ * Written by Sean Robbins in October 2019                              *
+ *                                                                      *
+ * The purpose of VRLever.cs is to serve as a basic lever setup script  *
+ * to be used on any lever in the scene.  It's meant to be as simple as *
+ * possible to run quickly and be intuitive.                            *
+ *                                                                      *
+ * Edited by Nicholas Mirchandani on 10/29/19                           *
+ ************************************************************************/
+
+using UnityEngine;
+using VRTK.GrabAttachMechanics;
+using UnityEngine.Events;
+
+public class VRLever : MonoBehaviour {
+    bool minEnabled;
+    bool maxEnabled;
+    public UnityEvent MaxAngleReached;
+    public UnityEvent MinAngleReached;
+
+    // Start is called before the first frame update
+    void Start() {
+        //Subscribes OnMaxAngleReached and OnMinAngleReached to VRTK's built in MaxAngleReached and MinAngleReached events, respectively
+        GetComponent<VRTK_RotateTransformGrabAttach>().MaxAngleReached += OnMaxAngleReached;
+        GetComponent<VRTK_RotateTransformGrabAttach>().MinAngleReached += OnMinAngleReached;
+        minEnabled = true;
+        maxEnabled = true;
+    }
+
+    //Runs when lever is fully rotated towards its maximum angle
+    void OnMaxAngleReached(object sender, RotateTransformGrabAttachEventArgs e) {
+        Debug.Log("Max Angle Reached");
+        //Ensures our MaxAngleReached UnityEvent is only called once per lever flip
+        GetComponent<VRTK_RotateTransformGrabAttach>().MaxAngleReached -= OnMaxAngleReached;
+        maxEnabled = false;
+        if(!minEnabled) {
+            GetComponent<VRTK_RotateTransformGrabAttach>().MinAngleReached += OnMinAngleReached;
+            minEnabled = true;
+        }
+        MaxAngleReached.Invoke();
+    }
+
+    //Runs when lever is fully rotated towards its minimum angle
+    void OnMinAngleReached(object sender, RotateTransformGrabAttachEventArgs e) {
+        Debug.Log("Min Angle reached");
+        //Ensures our MinAngleReached UnityEvent is only called once per lever flip
+        GetComponent<VRTK_RotateTransformGrabAttach>().MinAngleReached -= OnMinAngleReached;
+        minEnabled = false;
+        if(!maxEnabled) {
+            GetComponent<VRTK_RotateTransformGrabAttach>().MaxAngleReached += OnMaxAngleReached;
+            maxEnabled = true;
+        }
+        MinAngleReached.Invoke();
+    }
+
+}
