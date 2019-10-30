b3c282f12e3ba516fd3bda4b9e5d4d2874945af6
Nicholas Mirchandani <nmirchandani@chapman.edu>
2019-10-29 20:45:31 -0700

    Adds CockpitManager

---
+++ Assets/CockpitManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CockpitManager : MonoBehaviour
+{
+    //Mutator for target planet
+    public void SetTargetPlanet(string planetName)
+    {
+        GameManager.Instance.SetTargetPlanet(planetName);
+    }
+
+    //Beams to target planet
+    public void BeamToPlanet()
+    {
+        GameManager.Instance.BeamToPlanet();
+    }
+}
