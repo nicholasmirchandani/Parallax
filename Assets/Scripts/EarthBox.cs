dd2dbc13a893a92118699e05e005b62e73a19256
Dan Haub <haub@chapman.edu>
2019-11-02 15:34:51 -0700

    debugged earth box, changed controller tags on all scenec, made template scene with pre tagged controller objects

--- Assets/Scripts/EarthBox.cs
+++ Assets/Scripts/EarthBox.cs
@@ -9,6 +9,7 @@
  * An object leaves the box, this consant force component is destroyed  *
  *                                                                      *
  * Updated by Ryan Millares on 11/01/19                                 *
+ * Updated by Dan Haub on 11/2/19                                       *
  ************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
@@ -20,22 +21,18 @@ public class EarthBox : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
         string colliderName = other.gameObject.name;
-        if (colliderName.Substring(0,14).Equals("[VRTK][AUTOGEN]") || other.gameObject.tag == "Hand") { //to exclude the controllers from receiving ConstantForce
-            return;
+        if (!((colliderName.Length > 15) && colliderName.Substring(0,15).Equals("[VRTK][AUTOGEN]")) && !(other.gameObject.tag == "Hand")) { //to exclude the controllers from receiving ConstantForce
+            Debug.Log("In Earth Box [tag: \"" + other.gameObject.tag + "\", name: \"" + colliderName + "\"]");
+            ConstantForce cf = other.gameObject.AddComponent<ConstantForce>();
+            cf.force = new Vector3(0F, GameManager.Instance.GetDiffToEarthGravity(), 0F);
         }
-        ConstantForce cf = other.gameObject.AddComponent<ConstantForce>();
-        cf.force = new Vector3(0F, GameManager.Instance.GetDiffToEarthGravity(), 0F);
-
-
-
     }
+
     private void OnTriggerExit(Collider other) {
-        
-    ConstantForce cf = other.gameObject.GetComponent<ConstantForce>();
+        ConstantForce cf = other.gameObject.GetComponent<ConstantForce>();
         if (cf == null) {
             return;
         }
         Destroy(cf);
     }
-    
 }
