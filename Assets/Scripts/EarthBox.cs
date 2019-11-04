fb616a719b8bc629a41e9b2cd4782aacb56073ef
Nicholas Mirchandani <nmirchandani@chapman.edu>
2019-11-03 17:09:32 -0800

    Prevents Controllers from breaking when entering the EarthBox by checking the parent for [VRTK][AUTOGEN]

--- Assets/Scripts/EarthBox.cs
+++ Assets/Scripts/EarthBox.cs
@@ -10,6 +10,7 @@
  *                                                                      *
  * Updated by Ryan Millares on 11/01/19                                 *
  * Updated by Dan Haub on 11/2/19                                       *
+ * Updated by Nicholas Mirchandani on 11/3/19                           *
  ************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
@@ -22,8 +23,9 @@ public class EarthBox : MonoBehaviour {
     //Runs when an object enters the earth box, adds a constant force
     private void OnTriggerEnter(Collider other) {
         string colliderName = other.gameObject.name;
-        if (!((colliderName.Length > 15) && colliderName.Substring(0,15).Equals("[VRTK][AUTOGEN]")) && !(other.gameObject.tag == "Hand")) { //to exclude the controllers from receiving ConstantForce
-            Debug.Log("In Earth Box [tag: \"" + other.gameObject.tag + "\", name: \"" + colliderName + "\"]");
+        string colliderParentName = other.gameObject.transform.parent.name;
+        if (!((colliderName.Length > 15) && colliderName.Substring(0,15).Equals("[VRTK][AUTOGEN]") || (colliderParentName.Length > 15) && colliderParentName.Substring(0,15).Equals("[VRTK][AUTOGEN]")) && !(other.gameObject.tag == "Hand")) { //to exclude the controllers from receiving ConstantForce
+            Debug.Log("In Earth Box [tag: \"" + other.gameObject.tag + "\", name: \"" + colliderName + "\" " + "Parent name: " + colliderParentName);
             ConstantForce cf = other.gameObject.AddComponent<ConstantForce>();
             cf.force = new Vector3(0F, GameManager.Instance.CalcDiffToEarthGravity(), 0F);
         }
