cd581ad3c54b1629453f3f84a43bf8345fba85cc
RyanMillares <rmillares@chapman.edu>
2019-11-08 17:46:58 -0800

    working on the popups

--- Assets/Scripts/ShowPopups.cs
+++ Assets/Scripts/ShowPopups.cs
@@ -18,7 +18,14 @@ public class ShowPopups : MonoBehaviour {
     public GameObject[] interacts;
     private Vector3 dir;
     private float dotProd;
-
+    private Vector3 adjustPos = new Vector3(0F, 0.5F, 0F);
+    private Vector3 pos;
+    private Vector3 currentRot;
+    public float range;
+    private GameObject clone;
+    private Vector3 newRot;
+    private float x, y, z;
+    
 
     // Start is called before the first frame update
     void Awake() {
@@ -34,11 +41,14 @@ public class ShowPopups : MonoBehaviour {
     
     bool CheckInView() {
         
-        foreach (GameObject interact in interacts){ 
-            dir = (interact.transform.position - transform.position).normalized; //gets the vector from player to object
-  
+        foreach (GameObject interact in interacts){
+
+            pos = interact.transform.position;
+            dir = (pos - transform.position).normalized; //gets the vector from player to object
+            
             dotProd = Vector3.Dot(dir, transform.forward); //calculates the dot product wait...
-            if(dotProd > 0.99) { 
+            if(dotProd > range) {
+               
                 return true;
             }
 
@@ -46,11 +56,36 @@ public class ShowPopups : MonoBehaviour {
         return false;
     }
     void Popup() {
+
         if (CheckInView()) { 
-            popup.SetActive(true);//show the popup if looking at/near the object
+            ShowOver();//show the popup if looking at/near the object
         }
         else {
-            popup.SetActive(false); //hide it once player looks away
+            if(clone != null){
+                Destroy(clone);
+            }
+            //hide it once player looks away
         }
     }
+    void ShowOver() {
+        float newX, newY, newZ;
+        pos += adjustPos;
+        x = transform.forward.x;
+        y = transform.forward.y;
+        z = transform.forward.z;
+        newX = (Mathf.Atan2(Mathf.Sqrt(Mathf.Pow(y, 2) + Mathf.Pow(z, 2)), x))*180 / Mathf.PI;
+        newY = (Mathf.Atan2(Mathf.Sqrt(Mathf.Pow(x, 2) + Mathf.Pow(z, 2)), y))*180 / Mathf.PI;
+        newZ = (Mathf.Atan2(Mathf.Sqrt(Mathf.Pow(y, 2) + Mathf.Pow(x, 2)), z))*180 / Mathf.PI;
+        newRot = new Vector3(newX - 180F, newY - 90F, 0);
+        Debug.Log(newX);
+        Debug.Log(newY);
+        Debug.Log(newZ);
+
+
+
+        currentRot = new Vector3(-GameObject.Find("Neck").transform.rotation.x, this.transform.rotation.y - 90, 0);
+        clone = Instantiate(popup, pos, Quaternion.Euler(newRot - new Vector3(0F, 90F, 0F)));
+        
+    }
+    
 }
