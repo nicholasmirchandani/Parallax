58457944c4c071eb4f70dcca001893243284b054
Nicholas Mirchandani <nmirchandani@chapman.edu>
2019-11-17 17:49:39 -0800

    Begins to add art assets and configure scale but scale is broken

---
+++ Assets/Scripts/ScaleScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScaleScreen : MonoBehaviour
+{
+    public MeasureMass mMass;
+    public Text screenText;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        screenText.text = (mMass.totalMass * GameManager.Instance.currentGravity * -1).ToString();
+        Debug.Log("Total weight is " + mMass.totalMass * GameManager.Instance.currentGravity * -1); //Brute forcing a weight reading every 
+    }
+}
