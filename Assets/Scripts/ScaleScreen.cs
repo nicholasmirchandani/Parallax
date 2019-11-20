a4b568f041aea6d801b9ec3e5cbe87d858725ac3
Nicholas Mirchandani <nmirchandani@chapman.edu>
2019-11-19 21:47:39 -0800

    Final pre alpha for presentation

--- Assets/Scripts/ScaleScreen.cs
+++ Assets/Scripts/ScaleScreen.cs
@@ -16,7 +16,7 @@ public class ScaleScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        screenText.text = (mMass.totalMass * GameManager.Instance.currentGravity * -1).ToString();
+        screenText.text = (mMass.totalMass * GameManager.Instance.currentGravity * -1).ToString() + " lbs";
         Debug.Log("Total weight is " + mMass.totalMass * GameManager.Instance.currentGravity * -1); //Brute forcing a weight reading every 
     }
 }
