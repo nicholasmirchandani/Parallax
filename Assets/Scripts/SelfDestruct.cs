fa6a412de86c017a6ba2271715ed98de01244102
RyanMillares <rmillares@chapman.edu>
2019-11-24 17:33:33 -0800

    Particle Effect will self-destruct now

---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,20 @@
+﻿/************************************************************************
+ * Written by Ryan Millares on November 2019                            *
+ *                                                                      *
+ * The purpose of SelfDestruct.cs is to allow for instantiated particle *
+ * effects to destroy themselves after a specified delay to prevent     *
+ * game objects from cluttering the hierarchy from multiple teleports.  *
+ *                                                                      *
+ ************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+    public float delay;
+    // Start is called before the first frame update
+    void Start() {
+        Destroy(gameObject, delay);
+    }
+
+}
