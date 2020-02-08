dbd7b88d0d2270ed5d09dbc333dd271df8c57df9
Sean Robbins <srobbins@chapman.edu>
2020-02-08 13:19:28 -0800

    Rock Breakdown script

---
+++ Assets/Scripts/BreakDown.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakDown : MonoBehaviour {
+    public GameObject smallPrefab;
+    public Transform[] desiredPositions;
+    // Start is called before the first frame update
+    public void Breakdown() {
+        for(int i = 0; i < desiredPositions.Length; ++i) {
+            GameObject part = GameObject.Instantiate(smallPrefab);
+            part.transform.position = desiredPositions[i].position;
+        }
+        Destroy(gameObject);
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.J)) {
+            Breakdown();
+        }
+    }
+}
