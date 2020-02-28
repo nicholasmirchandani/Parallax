e24d4617d4ffe2cd3a98b4f2142b45d5b31e7aca
Dan Haub <haub@chapman.edu>
2020-02-27 21:05:13 -0800

    refactors audio manager code

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,9 +1,8 @@
 ﻿/************************************************************************
- * Cowritten by Ryan Millares on January 2020                           *
+ * Cowritten by Ryan Millares, Nick Mirchandani, and Dan Haub 2/2020    *
  *                                                                      *
  * The purpose of AudioManager.cs is to provide functionality for the   *
- * management of audio clips and accompanying subtitle texts, as well   *
- * as making sure     *
+ * management of audio clips and accompanying subtitle texts.           *
  *                                                                      *
  ************************************************************************/
 using System;
@@ -11,116 +10,83 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour {
+    #region variables
     public Text subtitleDisplay;
-    bool isPlay = false;
     public Audiofile[] audioFiles;
-    private AudioSource sound;
-    private float timestamp = 0;
-    private bool lineAssigned = false;
-    private int theIndex;
-    private int currentindex = 0;
 
+    private AudioSource source;
+    private Audiofile currentFile;
+    private float timestamp = 0;
+    private int subtitleIndex = 0;
+    private bool subtitleSwitched = true;
+    #endregion
+
+    #region structs
     [System.Serializable] public struct Subtitle {
         public string line;
         public float startTime;
         public float endTime;
     }
-
+    
     [System.Serializable] public struct Audiofile {
         public string name;
         public AudioClip clip;
         public Subtitle[] subtitleList;
     }
-  
+    #endregion
 
+    #region methods
     // Start is called before the first frame update
     void Awake(){
-        sound = GetComponent<AudioSource>();
-        
+        source = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
+    // Ensures subtitles are updated each fame
     void Update() {
-        //DEBUG
-        if(Input.GetKeyDown(KeyCode.J)) {
-            PlayFile(0);
+        if (source.isPlaying) {
+            timestamp += Time.deltaTime;
+            UpdateSubtitle(currentFile.subtitleList);
+        }
+    }
+
+    //Plays an audio file by name and displays its subtitles
+    public void PlayFile(string name) {
+        if(source.isPlaying) {
+            source.Stop();
         }
 
-        if (sound.isPlaying) {
-            timestamp += Time.deltaTime;
-            UpdateText(sound, audioFiles[theIndex].subtitleList);
-            
-            if (!sound.isPlaying) {
-                timestamp = 0;
-                currentindex = 0;
-                lineAssigned = false;
+        //resets the values used for subtitles
+        timestamp = 0;
+        subtitleIndex = 0;
+        subtitleSwitched = true;
+        subtitleDisplay.text = "";
+
+        //searches for the next subtitle by name
+        currentFile = Array.Find<Audiofile>(audioFiles, x => x.name == name);
+        source.clip = currentFile.clip;
+        source.Play();
+        UpdateSubtitle(currentFile.subtitleList);
+
+    }
+
+    //Updates the subtitle being displayed
+    private void UpdateSubtitle(Subtitle[] subtitles) {
+        //ensures there are more subtitles remaining
+        if(subtitleIndex < subtitles.Length) {
+            //checks if the current time stamp is within the range for the current subtitle index
+            if (subtitles[subtitleIndex].startTime <= timestamp && timestamp <= subtitles[subtitleIndex].endTime) {
+                subtitleDisplay.text = subtitles[subtitleIndex].line;
+                subtitleSwitched = false;
+            }
+            //increments subtitle index in between subtitles only once
+            else{
+                subtitleDisplay.text = "";
+                if(subtitleIndex < subtitles.Length && !subtitleSwitched) {
+                    ++subtitleIndex;
+                    subtitleSwitched = true;
+                }
             }
         }
     }
-
-    public void PlayFile(int index) {
-        subtitleDisplay.text = "";
-        theIndex = index;
-        Debug.Log(audioFiles[index].name);
-        for(int i = 0; i < audioFiles.Length; i++) {
-            Debug.Log(audioFiles[i].name);
-        }
-        Audiofile soundFile = audioFiles[index];
-        Debug.Log(soundFile.clip.name);
-        //Debug.Log(sound);
-
-        
-
-        sound.PlayOneShot(soundFile.clip);
-        UpdateText(sound, audioFiles[index].subtitleList);
-    }
-
-    public void UpdateText(AudioSource AS, Subtitle[] texts) {
-        
-        //bool isDone = false;
-        //float timestamp = 0;
-        //int currentindex = 0;
-        //while (AS.isPlaying) {
-        if((currentindex != texts.Length)) {
-        
-
-         
-
-
-               
-                if (timestamp >= texts[currentindex].startTime && !lineAssigned) {
-                    Debug.Log("Line set!");
-                    Debug.Log(texts[currentindex].startTime);
-                    subtitleDisplay.text = texts[currentindex].line;
-                    lineAssigned = true;    
-
-                }
-                else if (timestamp >= texts[currentindex].endTime && lineAssigned) {
-                    Debug.Log("Line Removed!");
-                    Debug.Log(texts[currentindex].endTime);
-
-                    subtitleDisplay.text = "";
-                    lineAssigned = false;
-                    if(currentindex == texts.Length) {
-                        //isDone = true;
-                    }
-                    else {
-                        ++currentindex;
-
-                    }
-
-                }
-           }
-       
-        //}
-
-            
-
-       
-        
-    }
-
-
-
-
+    #endregion
 }
