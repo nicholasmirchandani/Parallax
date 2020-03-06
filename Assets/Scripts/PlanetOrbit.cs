5d836dcf293081ae20fb2638dd8c6368df0f5eb3
nicholasmirchandani <nmirchandani@chapman.edu>
2020-03-05 20:06:19 -0800

    TrailRenderer tweaking

--- Assets/Scripts/PlanetOrbit.cs
+++ Assets/Scripts/PlanetOrbit.cs
@@ -7,7 +7,6 @@ public class PlanetOrbit : MonoBehaviour
     [SerializeField] private float HorizontalAxisLength;
     [SerializeField] private float VerticalAxisLength;
     [SerializeField] private float timeInEarthYears;
-    [SerializeField] private TrailRenderer trail;
     private float PlanetScale;
     private float HorizontalAxisLengthExpanded;
     private float VerticalAxisLengthExpanded;
@@ -34,19 +33,12 @@ public class PlanetOrbit : MonoBehaviour
 
     public void Orbit(float t, Vector3 origin) {
         if(lineUp) {
-            
-            if (trail.emitting){
-                trail.emitting = false;
-                trail.Clear();
-            }
-            
             if(currentT > Mathf.PI) {
                 currentT = Mathf.Lerp(currentT, 2 * Mathf.PI, 0.05f);
             } else {
                 currentT = Mathf.Lerp(currentT, 0, 0.05f);
             }
         } else {
-            trail.emitting = true;
             currentT = ((t / timeInEarthYears) + currentT) % (2 * Mathf.PI);
         }
 
@@ -55,46 +47,13 @@ public class PlanetOrbit : MonoBehaviour
             currentVerticalAxisLength = Mathf.Lerp(currentVerticalAxisLength, VerticalAxisLengthExpanded, 0.05f);
             currentPlanetScale = Mathf.Lerp(currentPlanetScale, PlanetScaleExpanded, 0.05f);
 
-            if(!Mathf.Approximately(currentHorzontalAxisLength, HorizontalAxisLengthExpanded) &&
-                !Mathf.Approximately(currentVerticalAxisLength, VerticalAxisLengthExpanded) &&
-                !Mathf.Approximately(currentPlanetScale, PlanetScaleExpanded)){
-                if (trail.emitting)
-                {
-                    trail.emitting = false;
-                    trail.Clear();
-                }
-            } else if(!lineUp){
-                trail.emitting = true;
-            }
-
         } else {
             currentHorzontalAxisLength = Mathf.Lerp(currentHorzontalAxisLength, HorizontalAxisLength, 0.05f);
             currentVerticalAxisLength = Mathf.Lerp(currentVerticalAxisLength, VerticalAxisLength, 0.05f);
             currentPlanetScale = Mathf.Lerp(currentPlanetScale, PlanetScale, 0.05f);
-
-            if (!Mathf.Approximately(currentHorzontalAxisLength, HorizontalAxisLength) &&
-                !Mathf.Approximately(currentVerticalAxisLength, VerticalAxisLength) &&
-                !Mathf.Approximately(currentPlanetScale, PlanetScale))
-            {
-                if (trail.emitting)
-                {
-                    trail.emitting = false;
-                    trail.Clear();
-                }
-            }
-            else if (!lineUp)
-            {
-                trail.emitting = true;
-            }
         }
         //Assuming planets are childed to controller
         transform.localPosition = new Vector3(currentHorzontalAxisLength * Mathf.Cos(currentT), 0, currentVerticalAxisLength * Mathf.Sin(currentT));
         transform.localScale = new Vector3(currentPlanetScale, currentPlanetScale, currentPlanetScale);
-
-        //transform.position = new Vector3(HorizontalAxisLength * Mathf.Cos(currentT) + origin.x, origin.y, VerticalAxisLength * Mathf.Sin(currentT) + origin.z);
-        /*
-        HorizontalAxisLength * Mathf.Cos(currentT);
-        VerticalAxisLength * Mathf.Sin(currentT);
-        */
     }
 }
